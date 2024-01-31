3d08ab437ea44c8b391d59ac3bc40c96756bfa8e
kouk02 <kouk20020703@gmail.com>
2024-01-31 03:38:37 +0200

    Fix folder/file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 // -- task 1 --
-import Profile from './Profile';
+import Profile from './Profile/Profile';
 import user from '../data/user.json';
 
 // -- task 2 --
-import Statistics from './Statistics'; 
+import Statistics from './Statistics/Statistics'; 
 import statisticsData from '../data/data.json';
 
 // -- task 3 --
-import FriendList from './FriendList';
+import FriendList from './FriendList/FriendList';
 import friendsData from '../data/friends.json';
 
 // -- task 4 --
-import TransactionHistory from './TransactionHistory';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 import transactions from '../data/transactions.json';
 
 
